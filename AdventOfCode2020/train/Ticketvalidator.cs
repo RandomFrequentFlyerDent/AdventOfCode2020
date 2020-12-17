84a0b859ba3a81b777592ee3daa7b75d0c04cb88
n.everts <nathalie.everts@isatis-health.nl>
2020-12-17 17:22:04 +0100

    doesn't seem like the ranges are a good idea...

--- AdventOfCode2020/train/Ticketvalidator.cs
+++ AdventOfCode2020/train/Ticketvalidator.cs
@@ -1,15 +1,70 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020.train
 {
     public class Ticketvalidator : ILogic
     {
+        private List<int> _myTicket = new List<int>();
+        private List<List<int>> _nearbyTickets = new List<List<int>>();
+        private ValidationRules _validationRules = new ValidationRules();
+
         public object GetAnswer(List<string> input, int part)
         {
+            ExtractRulesAndTickets(input);
             var answer = part == 1
                 ? 1
                 : 2;
             return answer;
+        }
+
+        //private List<int> GetInvalidByAnyField()
+        //{
+        //    var ranges = _validationRules.Rules.Values.SelectMany(d => d).ToList();
+        //    _nearbyTickets.SelectMany(nt => nt).Where(t => ranges.)
+        //}
+
+        private void ExtractRulesAndTickets(List<string> input)
+        {
+            var counter = 0;
+            var parsingRules = true;
+
+            Regex regex = new Regex(@"^(?<field>(\w+\s)?\w+): (?<first>\d+)-(?<second>\d+) or (?<third>\d+)-(?<fourth>\d+)");
+            do
+            {
+                Match match = regex.Match(input[counter]);
+                if (match.Success)
+                {
+                    var groups = match.Groups;
+                    _validationRules.AddToRules(groups["field"].Value, int.Parse(groups["first"].Value),
+                        int.Parse(groups["second"].Value), int.Parse(groups["third"].Value), int.Parse(groups["fourth"].Value));
+                    counter++;
+                }
+                else
+                {
+                    parsingRules = false;
+                }
+            } while (parsingRules);
+
+            for (int i = counter; i < input.Count; i++)
+            {
+                if (input[i].Equals("your ticket:"))
+                {
+                    _myTicket = input[i + 1].Split(',').Select(i => int.Parse(i)).ToList();
+                    counter = i + 1;
+                    break;
+                }
+            }
+
+            for (int i = counter; i < input.Count; i++)
+            {
+                if (!input[i].Equals("nearby tickets:"))
+                {
+                    var ticket = input[i].Split(',').Select(i => int.Parse(i)).ToList();
+                    _nearbyTickets.Add(ticket);
+                }
+            }
         }
     }
 }
---
+++ AdventOfCode2020/train/ValidationRules.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.train
+{
+    public class ValidationRules
+    {
+        public Dictionary<string, Range[]> Rules { get; }
+
+        public ValidationRules()
+        {
+            Rules = new Dictionary<string, Range[]>();
+        }
+
+        public void AddToRules(string field, int firstRangeStart, int firstRangeEnd, int secondRangeStart, int secondRangeEnd)
+        {
+            Rules.Add(field, new Range[] { new Range(firstRangeStart, firstRangeEnd), new Range(secondRangeStart, secondRangeEnd) });
+        }
+    }
+}
